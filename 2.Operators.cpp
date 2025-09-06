70520b5d75452bceebba665a24002e106f52d480
SakshiSupekar07 <supekarsakshj07@gmail.com>
2025-09-06 10:55:19 +0530

    Completed Day 2

---
+++ 2.Operators.cpp
@@ -0,0 +1,73 @@
+//Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost. Round the result to the nearest integer.
+
+//Example
+
+
+//A tip of 15% * 100 = 15, and the taxes are 8% * 100 = 8. Print the value and return from the function. 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'solve' function below.
+ *
+ * The function accepts following parameters:
+ *  1. DOUBLE meal_cost
+ *  2. INTEGER tip_percent
+ *  3. INTEGER tax_percent
+ */
+
+void solve(double meal_cost, int tip_percent, int tax_percent) {
+     double tip=meal_cost/100.0 *tip_percent;
+    double tax=tax_percent/100.0 *meal_cost;
+    float total_cost=meal_cost+tip+tax;
+     cout<<round(total_cost)<<endl;
+    
+}
+
+int main()
+{
+    string meal_cost_temp;
+    getline(cin, meal_cost_temp);
+
+    double meal_cost = stod(ltrim(rtrim(meal_cost_temp)));
+
+    string tip_percent_temp;
+    getline(cin, tip_percent_temp);
+
+    int tip_percent = stoi(ltrim(rtrim(tip_percent_temp)));
+
+    string tax_percent_temp;
+    getline(cin, tax_percent_temp);
+
+    int tax_percent = stoi(ltrim(rtrim(tax_percent_temp)));
+
+    solve(meal_cost, tip_percent, tax_percent);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
